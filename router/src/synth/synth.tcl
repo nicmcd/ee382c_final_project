a5244fc3dca941404660c4bb22f16864560c1a9c
Olivier Jin <ojin@myth4.stanford.edu>
2016-05-15 06:25:01 -0700

    Added synth.tcl

---
+++ router/src/synth/synth.tcl
@@ -0,0 +1,38 @@
+
+set search_path [concat $search_path .. ../clib]
+set target_library class.db
+set link_library   class.db
+
+#set name c_functions
+#set wild *
+#set c_files   [glob -path ../clib/ $wild$name$wild]
+
+#set c_files   [glob -path ../clib/ *]
+set c_files   []
+set rtr_files [glob -path ../ rtr*]
+#set rtr_files []
+set all_files [concat $c_files $rtr_files]
+set all_files [lindex $all_files last]
+
+foreach path $all_files {
+  set file_name [lindex [split $path /] end]
+  set design_name [lindex [split $file_name .] 0]
+
+  if {[string match $design_name "c_functions"]} {
+    continue
+  }
+
+  puts $file_name
+  read_file -f verilog $file_name
+  analyze   -f verilog $file_name
+  elaborate $design_name
+  compile_ultra
+
+  if {[string match $design_name "rtr_top"]} {
+    report_timing
+    report_area
+    report_power
+  }
+}
+
+exit

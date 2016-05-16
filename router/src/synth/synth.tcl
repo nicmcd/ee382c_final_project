c69954c477b52612597a8807e3b21f4fe2554b1d
Olivier Jin <ojin@corn03.stanford.edu>
2016-05-15 20:21:44 -0700

    synth.tcl with new flags

--- router/src/synth/synth.tcl
+++ router/src/synth/synth.tcl
@@ -3,24 +3,23 @@ set search_path [concat $search_path .. ../clib]
 set target_library class.db
 set link_library   class.db
 
-#set name c_functions
-#set wild *
-#set c_files   [glob -path ../clib/ $wild$name$wild]
+# set name c_lfsr
+# set wild *
+# set c_files   [glob -path ../clib/ $wild$name$wild]
 
-#set c_files   [glob -path ../clib/ *]
-set c_files   []
+set c_files   [glob -path ../clib/ *]
+# set c_files   []
 set rtr_files [glob -path ../ rtr*]
-#set rtr_files []
+# set rtr_files []
 set all_files [concat $c_files $rtr_files]
-set all_files [lindex $all_files last]
 
 foreach path $all_files {
   set file_name [lindex [split $path /] end]
   set design_name [lindex [split $file_name .] 0]
 
-  if {[string match $design_name "c_functions"]} {
-    continue
-  }
+  if {[string match $design_name "c_functions"  ]} { continue }
+  if {[string match $design_name "c_constants"  ]} { continue }
+  if {[string match $design_name "rtr_constants"]} { continue }
 
   puts $file_name
   read_file -f verilog $file_name
